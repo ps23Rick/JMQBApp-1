6e0254c47f7d9a35f3872849b4d680214bd32887
ps23Rick <all4jesus@mailbox.org>
2021-02-06 16:30:52 -0800

    Initial check-in

---
+++ JMApp1/JMApp1/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows;
+using Interop.QBFC13;
+// using Interop.QBFC14;
+using Zombie;
+//using QBFC14Lib;
+
+namespace JMApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            using (var cn = ConnectionMgr.GetConnection())
+            {
+                Console.WriteLine("Customer list from the current company file");
+
+                var batch = cn.NewBatch();
+
+                var qryCust = batch.MsgSet.AppendCustomerQueryRq();
+
+                batch.SetClosures(qryCust, b =>
+                {
+                    var customers = new QBFCIterator<ICustomerRetList, ICustomerRet>(b);
+
+                    foreach (var customer in customers)
+                    {
+                        // Console.WriteLine(Safe.Value(customer.FullName));
+                    }
+                });
+
+                batch.Run();
+            }
+
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Zombie/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Zombie")]
+[assembly: AssemblyDescription("Zombie open source library for QuickBooks")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Zombie")]
+[assembly: AssemblyCopyright("Copyright © 2003 - 2012 PJPM")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4e5b0372-5666-46bb-87ac-57bbda11fc47")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.2.4.0")]
